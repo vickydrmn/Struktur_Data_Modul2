4e47e0d2e4d210914aa2f6d7cc3cf332ae8506f0
vickydrmn <98814238+vickydrmn@users.noreply.github.com>
2023-03-24 08:10:17 +0700

    Add files via upload

---
+++ LatihanMod2.py
@@ -0,0 +1,13 @@
+Hewan = ["Sapi", "Kelinci", "Kambing", "Unta", "Domba"]
+print("Hewan : ")
+print(Hewan)
+
+Hewan_Del = Hewan.pop(-4)
+Hewan_De2 = Hewan.pop(-3)
+Hewan_De3 = Hewan.pop(-2)
+
+print("Hewan yang dihapus : ")
+print([Hewan_Del, Hewan_De2, Hewan_De3])
+
+print("Output Hewan : ")
+print(Hewan)
